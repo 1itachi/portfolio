c53b3d5bc94a8e6c8a5bf1b3561582c64157947e
deepak <kumar.de@husky.neu.edu>
2020-08-10 10:04:11 -0400

    first commit

---
+++ src/Components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class MainPage extends React.Component {
+render() {
+    return(
+        <div className = "MainPage">
+            This is the main page.
+    </div>
+    )
+}
+
+}
