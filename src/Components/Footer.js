7163be9d6001153eac77434683f7dcb84aa5db07
deepak <kumar.de@husky.neu.edu>
2020-08-19 21:48:34 -0400

    updated the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ export default class Contact extends React.Component {
         return(
             <div className="margin contact mb-4">
              
-                <p className="footer">Built by Deepak Kumar. Design Insipired by Brittany Chiang.</p>
+                <p className="footer">Built using React.Js by Deepak Kumar. Design insipired by Brittany Chiang.</p>
                 <a class="github " href="https://github.com/itachi1994/portfolio" target="_blank" rel="noopener noreferrer"><i class="fab fa-github fa-2x"></i></a>
             </div>
         );
